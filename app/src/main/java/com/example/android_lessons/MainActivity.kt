03ec885550b0eede9bf3a30c64c72290de85f798
Malik <bittiev.malik@mail.ru>
2024-11-08 00:04:28 +0300

    second commit download image from internet with the picasso addiction

--- app/src/main/java/com/example/android_lessons/MainActivity.kt
+++ app/src/main/java/com/example/android_lessons/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.core.graphics.drawable.toIcon
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.snackbar.Snackbar
+import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,22 +18,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val image = findViewById<ImageView>(R.id.imageload)
-        image.setImageResource(R.drawable.ic_geg)
-        val loadimage = DownloadImage(URL,object: ImageCallBack{
-            override fun success(bitmap: Bitmap) {
-                image.setImageBitmap(bitmap)
-            }
 
-            override fun failed() {
-                Snackbar.make(image,"failed",Snackbar.LENGTH_SHORT).show()
-            }
+        Picasso.get().load(URL).centerCrop()
+            .resize(720,1280)
+            .placeholder(android.R.drawable.ic_media_pause)
+            .error(android.R.drawable.ic_dialog_alert)
+            .into(image)
 
-        })
-    loadimage.start()
     }
 
 private companion object {
-    const val  URL = "https://static.wikia.nocookie.net/anime-characters-fight/images/3/31/Satoru_5003.png/revision/latest?cb=20210404115448&path-prefix=ru"
+    const val  URL = "https://sun9-20.userapi.com/impf/c853528/v853528468/1c60b/3vtdaJa1oDY.jpg?size=1442x2160&quality=96&sign=ab1b455cff5f5f8bbf359a84773133bd&type=album"
 }
 }
 
